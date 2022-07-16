7c00aed24e2704985813a73baf5af6c989e24729
muhmdamin <mohamedamin.fcih@gmail.com>
2022-07-16 09:52:27 +0200

    edit ec2 code

--- ec2.tf
+++ ec2.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "bastion" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id = aws_subnet.lab1-public-subnet1.id
-  security_groups = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   key_name = aws_key_pair.my-private-key.key_name
   associate_public_ip_address = true
 
@@ -31,7 +31,7 @@ resource "aws_instance" "application" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id = aws_subnet.lab1-private-subnet1.id
-  security_groups = [aws_security_group.allow_ssh_and_open3000.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh_and_open3000.id]
   key_name = aws_key_pair.my-private-key.key_name
   tags = {
     Name = "application"
